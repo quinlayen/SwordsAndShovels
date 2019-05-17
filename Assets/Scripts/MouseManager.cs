8d6f061109eb802874c04c94ac579d46d948660b
Peter Faso <pfaso@avatar-reality.com>
2019-05-16 23:53:08 -1000

    first commit.  Mats complete, world blocked out.

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    //Know what objects are clickable
+    public LayerMask clickableLayer;
+
+    //Swap cursors per object type
+    public Texture2D pointer; //normal pointer
+    public Texture2D target; //clickable objects cursor like the world
+    public Texture2D doorway; //cursor for doorways
+    public Texture2D combat; //cursror for combat actions
+
+    public EventVector3 OnClickEnvironment;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            bool item = false;
+
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+
+            else if (hit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combat, new Vector2(16, 16), CursorMode.Auto);
+                item = true;
+            }
+
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (door)
+                {
+                    Transform doorway = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(doorway.position);
+                    Debug.Log("Door has been clicked");
+                }
+
+                else if (item)
+                {
+                    Transform targetItem = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(targetItem.position);
+                    Debug.Log("You clicked an item");
+                }
+
+                else
+                {
+                    OnClickEnvironment.Invoke(hit.point);
+                }
+
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+    public float patrolTime = 10f;
+    public float aggroRange = 10f;
+    public Transform[] waypoints;
+
+    private int index;
+    private float speed, agentSpeed;
+    private Transform player;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        {
+            //anim = GetComponent<Animator>();
+            agent = GetComponent<NavMeshAgent>();
+            if (agent != null) { agentSpeed = agent.speed; }
+
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            index = Random.Range(0, waypoints.Length);
+
+            InvokeRepeating("Tick", 0, 0.5f);
+
+            if (waypoints.Length > 0) { InvokeRepeating("patrol", 0, patrolTime); }
+        }
+    }
+
+    private void patrol()
+    {
+        index = index == waypoints.Length - 1 ? 0 : index + 1; // if true then 0 else index + 1
+    }
+
+    private void Tick()
+    {
+        agent.speed = agentSpeed / 2;
+        agent.destination = waypoints[index].position;
+        if (player != null && Vector3.Distance(transform.position, player.position) < aggroRange)
+        {
+            Debug.Log("Close");
+            agent.destination = player.position;
+            agent.speed = agentSpeed;
+        }
+
+    }
+}
